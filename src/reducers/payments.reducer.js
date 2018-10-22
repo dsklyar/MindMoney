b07be550478311fb93e600ac56ceacc0ab75be76
unknown <danielksklyar@gmail.com>
2018-10-21 21:17:04 -0700

    example of reducer sanity check

--- src/reducers/payments.reducer.js
+++ src/reducers/payments.reducer.js
@@ -1,5 +1,9 @@
 import { SAVE_PAYMENT } from "../actions/types";
 const payments = (state = [], action) => {
+  // Sanity check here
+  if (action.type === undefined) {
+    throw new Error("Payment reducer recieved an undefined action!");
+  }
   switch(action.type) {
     case SAVE_PAYMENT: {
       return [
