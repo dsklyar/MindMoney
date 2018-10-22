2059a528a73685e36f8442192fa0e46933a1a0ac
unknown <danielksklyar@gmail.com>
2018-10-21 21:00:57 -0700

    removed baked in state for payments reducer

--- src/reducers/payments.reducer.js
+++ src/reducers/payments.reducer.js
@@ -1,21 +1,13 @@
-const def = [
-  { id: 1, amount: 1800, date: new Date(), comment: "Payment for October" },
-  { id: 2, amount: -100, date: new Date(), comment: "Car payment" },
-  { id: 3, amount: -300, date: new Date(), comment: "PS4 Pro Payment" }
-];
-
-const payments = (state = def, action) => {
+const payments = (state = [], action) => {
   console.log(action);
   switch(action.type) {
     case "SAVE_PAYMENT": {
-      console.log("save?")
       return [
         ...state,
         action.payload
       ];
     }
     default: {
-      console.log("return?")
       return state;
     }
   }
