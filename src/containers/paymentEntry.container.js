661f79ef38f0da6904af655fc5e93ba28ce5bdf6
unknown <danielksklyar@gmail.com>
2018-10-29 23:32:55 -0700

    added sliding animation

---
+++ src/containers/paymentEntry.container.js
@@ -0,0 +1,86 @@
+import React, {Component} from "react";
+import { Animated, View, PanResponder } from 'react-native';
+import { PHONE_DIMENSIONS } from "../constants";
+import {
+  ListItem,
+  Text,
+  Right,
+  Body,
+  Left,
+  Icon,
+  Card
+} from "native-base";
+
+
+export default class PaymentEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      translateX: new Animated.Value(0)
+    }
+    this.pan = PanResponder.create({
+      onMoveShouldSetPanResponderCapture: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, {dx: this.state.translateX}]),
+      onPanResponderRelease: (e, {vx, dx}) => {
+        const screenWidth = PHONE_DIMENSIONS.width;
+        if (Math.abs(vx) >= 0.5 || Math.abs(dx) >= 0.5 * screenWidth) {
+          Animated.timing(this.state.translateX, {
+            toValue: dx > 0 ? screenWidth : -screenWidth,
+            duration: 200
+          }).start(/*this.props.onDismiss*/);
+        } else {
+          Animated.spring(this.state.translateX, {
+            toValue: 0,
+            bounciness: 10
+          }).start();
+        }
+      }
+    });
+  }
+  render() {
+    return (
+      <View style={{
+        position: "relative"
+      }}>
+        {/* <View style={{
+          position: "absolute", 
+          backgroundColor: "red",
+          top: 0,
+          right: 0,
+          width: PHONE_DIMENSIONS.width,
+          height: 50,
+          zIndex: -1
+        }}>
+
+        </View> */}
+        <Animated.View
+          style={{
+            transform: [{translateX: this.state.translateX}],
+            height: 75
+          }}
+          {...this.pan.panHandlers}
+        >
+          <ListItem 
+            avatar
+            >
+            <Left>
+              <Icon type="Foundation" name="dollar" />
+              <Icon name={(this.props.payment.amount < 0) ? "arrow-down" : "arrow-up"} />
+            </Left>
+            <Body>
+              <Text>{this.props.payment.amount}</Text>
+              <Text note>{this.props.payment.comment}</Text>
+            </Body>
+            <Right>
+              <Text note>
+                {new Date(this.props.payment.date).toLocaleDateString("en-US")}
+              </Text>
+            </Right>
+          </ListItem>
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
--- src/containers/paymentsList.container.js
+++ src/containers/paymentsList.container.js
@@ -2,40 +2,22 @@ import React, { Component } from "react";
 import {
   Container,
   Content,
-  List,
-  ListItem,
-  Text,
-  Right,
-  Body,
-  Left,
-  Icon
+  List
 } from "native-base";
 import { connect } from "react-redux";
+import PaymentEntry from "../containers/paymentEntry.container"
 
 class PaymentsList extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <Container>
         <Content>
           <List
             dataArray={this.props.payments}
-            renderRow={payment => (
-              <ListItem avatar>
-                <Left>
-                  <Icon type="Foundation" name="dollar" />
-                  <Icon name={(payment.amount < 0) ? "arrow-down" : "arrow-up"} />
-                </Left>
-                <Body>
-                  <Text>{payment.amount}</Text>
-                  <Text note>{payment.comment}</Text>
-                </Body>
-                <Right>
-                  <Text note>
-                    {new Date(payment.date).toLocaleDateString("en-US")}
-                  </Text>
-                </Right>
-              </ListItem>
-            )}
+            renderRow={payment => (<PaymentEntry payment={payment}/>)}
           />
         </Content>
       </Container>
